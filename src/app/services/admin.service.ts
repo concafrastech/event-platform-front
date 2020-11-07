24dce90699d67f77e882cf0c9be26b70aa59d113
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2020-11-07 14:29:37 -0300

    Arrumando chamada api

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -1,34 +1,28 @@
 import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { GLOBAL } from './global';
+import { UserService } from './user.service';
 
 @Injectable()
 export class AdminService {
     public url: String;
     public token;
 
-    constructor(public _http: HttpClient ) { 
+    constructor(
+        public _http: HttpClient,
+        private _userService: UserService
+    ) { 
         this.url = GLOBAL.url;
-    }
-
-    getToken() {
-        let token = localStorage.getItem('token');
-
-        if (token != "undefined") {
-            this.token = token;
-        } else {
-            this.token = null;
-        }
-
-        return this.token;
     }
 
     helloWorld() {
         console.log("Hello World");
-        // let headers = new HttpHeaders()
-        //     .set('Content-Type', 'application/json')
-        //     .set('Authorization', this.getToken());
+        this._userService
+        let headers = new HttpHeaders()
+            .set('Content-Type', 'application/json')
+            .set('Authorization', this._userService.getToken());
 
-        // return this._http.get(this.url + "admin", {headers: headers});
+        console.log(headers);
+        return this._http.get(this.url + "admin", {headers: headers});
     }
 }

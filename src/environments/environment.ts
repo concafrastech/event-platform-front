2e31d9c50aa9f0f223426d0d4529ef3e66b2a571
Marcel Ferry <marcelferry@gmail.com>
2020-11-09 22:41:52 -0300

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:5001/event-platform-concafras-back/us-central1/app/api/',
-  signatureEndpoint: 'http://localhost:4000',
+  signatureEndpoint: 'https://event-platform-zoom.herokuapp.com',
   leaveUrl: 'http://localhost:4200'
 };
 

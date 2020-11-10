7d62db2075a58c889de12881194055878763c389
Marcel Ferry <marcelferry@gmail.com>
2020-11-09 23:17:06 -0300

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://apieventos.contratacaodemetas.com.br/api/',
   signatureEndpoint: 'https://event-platform-zoom.herokuapp.com',
-  leaveUrl: 'http://event-platform-concafras.web.app'
+  leaveUrl: 'https://event-platform-concafras.web.app'
 };

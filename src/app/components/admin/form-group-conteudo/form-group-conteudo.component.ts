8fb6ca64205bcb3c03466519fd36668dce996163
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2020-11-24 22:03:37 -0300

    Corrigindo bugs css

--- src/app/components/admin/form-group-conteudo/form-group-conteudo.component.ts
+++ src/app/components/admin/form-group-conteudo/form-group-conteudo.component.ts
@@ -4,18 +4,18 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   selector: 'form-group-conteudo',
   styleUrls: [`./form-group-conteudo.component.css`],
   template: `
-    <div class="form-group row">
+    <div class="form-group">
       <h3>{{ content.title }}</h3>
-      <div *ngFor="let input of content.inputs" class="row">
-        <label for="{{ 'input' + content.id + input.name }}" class="col-sm-2 col-form-label padd10-vert">{{ input.label }}</label>
-        <div class="col-sm-10 padd5">
+      <div *ngFor="let input of content.inputs" class="gamb5">
+        <label for="{{ 'input' + content.id + input.name }}" class="col-sm-2 col-form-label">{{ input.label }}</label>
+        <div class="col-sm-10">
           <input type="{{ input.type }}" class="form-control" id="{{ 'input' + content.id + input.name}}" placeholder="{{ input.placeholder }}">
         </div>
       </div>
-      <div class="row padd5">
+      <div class="padd5">
         <input class="form-control" type="text" placeholder="Ordem: {{ ordem + 1 }}" readonly>
       </div>
-      <div class="row padd5">
+      <div class="padd5">
         <button class="btn btn-danger" (click)="removeType(content)"><i class="glyphicon glyphicon-trash"></i></button>
       </div>
     </div>

e26505e02d2eb87412495d5eadb7bcb1354c0f89
Fábio Santos <fabio@dblink.com.br>
2020-11-26 22:24:22 -0300

    iniciado integração do gerenciador de conteúdos com palestras.

--- src/app/services/content.service.ts
+++ src/app/services/content.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpRequest, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { GLOBAL } from "./global";
 import { Observable } from "rxjs/Observable";
@@ -21,4 +21,20 @@ export class ContentService {
 
     return this._http.post(this.url + "contents", params, { headers: headers });
   }
+
+  uploadFile(files: Set<File>): Observable<any>{
+    let formData = new FormData();
+    files.forEach((arquivo) => {
+      formData.append("arquivo", arquivo, arquivo.name);
+    })
+
+    let headers = new HttpHeaders()
+      .set("Content-Type", "multipart/form-data")
+      .set("Authorization", this._userService.getToken());
+
+    const requisicao = new HttpRequest("POST", this.url + "contents/uploads", formData, { headers: headers, reportProgress: true});
+    return this._http.request(requisicao);
+  }
+
+  
 }

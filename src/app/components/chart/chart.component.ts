7c995c704d98a89eb96791c163071e94a0346cb6
Fábio Santos <fabio@dblink.com.br>
2020-12-10 22:00:03 -0300

    exemplo servico

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { UserService } from "src/app/services/user.service";
 
 @Component({
   selector: "app-chart",
@@ -6,7 +7,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./chart.component.css"],
 })
 export class ChartComponent implements OnInit {
-  constructor() {}
+  constructor(private _userService: UserService) {}
 
   public barChartOptions = {
     scaleShowVerticalLines: false,
@@ -28,20 +29,27 @@ export class ChartComponent implements OnInit {
     { data: [28, 48, 40, 19, 86, 27, 90], label: "Series B" },
   ];
 
-  public doughnutChartLabels = ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'];
+  public doughnutChartLabels = ["Sales Q1", "Sales Q2", "Sales Q3", "Sales Q4"];
   public doughnutChartData = [120, 150, 180, 90];
-  public doughnutChartType = 'doughnut';
+  public doughnutChartType = "doughnut";
 
-  public radarChartLabels = ['Q1', 'Q2', 'Q3', 'Q4'];
+  public radarChartLabels = ["Q1", "Q2", "Q3", "Q4"];
   public radarChartData = [
-    {data: [120, 130, 180, 70], label: '2017'},
-    {data: [90, 150, 200, 45], label: '2018'}
+    { data: [120, 130, 180, 70], label: "2017" },
+    { data: [90, 150, 200, 45], label: "2018" },
   ];
-  public radarChartType = 'radar';
+  public radarChartType = "radar";
 
-  public pieChartLabels = ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'];
+  public pieChartLabels = ["Sales Q1", "Sales Q2", "Sales Q3", "Sales Q4"];
   public pieChartData = [120, 150, 180, 90];
-  public pieChartType = 'pie';
+  public pieChartType = "pie";
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    let totalRegistros = 0;
+    this._userService.getUsers().subscribe((response) => {
+      totalRegistros = response.total;
+      console.log(totalRegistros);
+      
+    });
+  }
 }

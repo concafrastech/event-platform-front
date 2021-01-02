8858c0f2d06c10d1fb9f9dbedb1c32e0186a57c0
Victor Teodoro <victor.atomo@gmail.com>
2021-01-02 17:42:04 -0300

    dashboard

--- src/app/epics/jovem/jovem-dashboard/jovem-dashboard.component.ts
+++ src/app/epics/jovem/jovem-dashboard/jovem-dashboard.component.ts
@@ -14,12 +14,10 @@ export class JovemDashboardComponent implements OnInit {
 
 }
 
-	$(document).ready(function () {
-		console.log("document is ready");
-		$('[data-toggle="offcanvas"], #navToggle').on('click', function () {
-			$('.offcanvas-collapse').toggleClass('open')
-		})
-	});
-	window.onload = function () {
-		console.log("window is loaded");
-	};
+$(document).ready(function() {
+  $("#sidebarCollapse").on("click", function() {
+    $("#sidebar").toggleClass("active");
+    $(this).toggleClass("active");
+  });
+});
+

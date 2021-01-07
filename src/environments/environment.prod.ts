48a9cbdf07bf89dfa6d0e4dded505479695dc1ae
Marcel Ferry <marcelferry@gmail.com>
2021-01-07 11:07:45 -0300

    fix zoom error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'https://event-platform-back-6aehl.ondigitalocean.app/api/',
   signatureEndpoint: 'https://event-platform-back-6aehl.ondigitalocean.app/api/zoom',
   leaveUrl: 'https://event-platform-concafras.web.app',
-  zoomUrl: 'https://event-platform-concafras-zoom.web.app'
+  zoomUrl: 'https://event-platform-concafras-zoom.web.app/home'
 };

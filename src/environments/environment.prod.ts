4a97b8911fe504ed56b5ee2e85d6fe85690730b3
Marcel Ferry <marcelferry@gmail.com>
2021-01-07 11:52:02 -0300

    Ajustes das urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://event-platform-back-6aehl.ondigitalocean.app/api/',
-  signatureEndpoint: 'https://event-platform-back-6aehl.ondigitalocean.app/api/zoom',
+  signatureEndpoint: 'https://event-platform-back-6aehl.ondigitalocean.app/zoom',
   leaveUrl: 'https://event-platform-concafras.web.app',
   zoomUrl: 'https://event-platform-concafras-zoom.web.app/home'
 };

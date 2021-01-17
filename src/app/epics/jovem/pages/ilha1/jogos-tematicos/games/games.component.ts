3c928ba0132c12185ece3f4fe2ddf2f521ba263d
Victor Teodoro <victor.atomo@gmail.com>
2021-01-17 14:01:02 -0300

    page game

--- src/app/epics/jovem/pages/ilha1/jogos-tematicos/games/games.component.ts
+++ src/app/epics/jovem/pages/ilha1/jogos-tematicos/games/games.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-games',
-  templateUrl: './games.component.html',
-  styleUrls: ['./games.component.css']
+	selector: 'app-games',
+	templateUrl: './games.component.html',
+	styleUrls: ['./games.component.css']
 })
 export class GamesComponent implements OnInit {
+	@Input() public jogo: string;
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+	constructor(
+		private _route: ActivatedRoute,
+		private _router: Router,
+		
+		) { }
+
+	ngOnInit(): void {
+		this._route.params.subscribe(params => {
+			let jogo = params['jogo'];
+			this.jogo = jogo;
+		});
+
+	}
 
 }

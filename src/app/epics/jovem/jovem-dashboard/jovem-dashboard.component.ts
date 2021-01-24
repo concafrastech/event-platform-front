39fc26307850dad0a04d0b62b96a901115d9274a
ivancpereirafilho <ivancpereirafilho@gmail.com>
2021-01-24 00:40:49 -0300

    Update jovem-list

--- src/app/epics/jovem/jovem-dashboard/jovem-dashboard.component.ts
+++ src/app/epics/jovem/jovem-dashboard/jovem-dashboard.component.ts
@@ -46,7 +46,6 @@
       };
       this.initGamefication();
   
-      console.log(this.user);
     }
   
     ngOnInit(): void {
@@ -60,80 +59,58 @@
     }
     
     initGamefication() {
-      console.log('gamification: ', this._gamificationService);
   
       this._gamificationService.addBreakpoint(100, () => {
-        console.log('breakpoint 100 callback: ', this._gamificationService.getPoints());
         this._bootstrapAlertService.alert(new NgBootstrapAlert("Parabéns você está em novo nível!", "alert-success"));
       });
       this._gamificationService.addBreakpoint(200, () => {
-        console.log('breakpoint 200 callback: ', this._gamificationService.getPoints());
         this._bootstrapAlertService.alert(new NgBootstrapAlert("Parabéns você está em novo nível!", "alert-success"));
       });
       this._gamificationService.addBreakpoint(300, () => {
-        console.log('breakpoint 300 callback: ', this._gamificationService.getPoints());
         this._bootstrapAlertService.alert(new NgBootstrapAlert("Parabéns você está em novo nível!", "alert-success"));
       });
       this._gamificationService.addBreakpoint(400, () => {
-        console.log('breakpoint 400 callback: ', this._gamificationService.getPoints());
         this._bootstrapAlertService.alert(new NgBootstrapAlert("Parabéns você está em novo nível!", "alert-success"));
       });
   
       let component = this._gamificationService.addComponent(400, () => {
-        console.info('component update callback');
         let points = this._gamificationService.getPoints();
         this._gamificationService.getLevelByPoints(points);
         this.user.points = points;
         this.user.level = this._gamificationService.getLevel();
         this.progress.value = points;
       }, () => {
-        console.log('component 400 start callback');
         this.progress.max = 3000;
         this.user.level = this._gamificationService.getLevel();
       });
   
   
       this._gamificationService.addMission('add', 50, '', () => {
-        console.log('add mission start');
       }, () => {
-        console.log('add mission achieve: ', this._gamificationService.getPoints());
       });
       
       this._gamificationService.addMission('timeline', 10, '', () => {
-        console.log('add mission start');
       }, () => {
-        console.log('add mission achieve: ', this._gamificationService.getPoints());
         this._bootstrapAlertService.alert(new NgBootstrapAlert("Você ganhou 10 pontos por acessar Timeline!", "alert-success"));
       });
       
       this._gamificationService.addMission('pessoas', 10, '', () => {
-        console.log('add mission start');
       }, () => {
-        console.log('add mission achieve: ', this._gamificationService.getPoints());
         this._bootstrapAlertService.alert(new NgBootstrapAlert("Você ganhou 10 pontos por acessar Pessoas!", "alert-success"));
       });
   
       this._gamificationService.addMission('mensagens', 10, '', () => {
-        console.log('add mission start');
       }, () => {
-        console.log('add mission achieve: ', this._gamificationService.getPoints());
         this._bootstrapAlertService.alert(new NgBootstrapAlert("Você ganhou 10 pontos por acessar Mensagens!", "alert-success"));
       });
   
       this._gamificationService.addMission('save', 30, '', () => {
-        console.log('save mission start');
       }, () => {
-        console.log('save mission achieve: ', this._gamificationService.getPoints());
       });
   
       this._gamificationService.addMission('delete', 10, '', () => {
-        console.log('delete mission start');
       }, () => {
-        console.log('delete mission achieve: ', this._gamificationService.getPoints());
       });
-  
-      console.log(this.user.level);
-      console.log(this.identity);
   
     }
   

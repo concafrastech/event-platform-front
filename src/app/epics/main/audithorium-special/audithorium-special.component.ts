a181b5bd5063eec816a5f93bfb349e36cafdade5
vinicius kremer <vinicius.kremer@gmail.com>
2021-01-26 21:03:59 -0300

    Add forms in buttons

--- src/app/epics/main/audithorium-special/audithorium-special.component.ts
+++ src/app/epics/main/audithorium-special/audithorium-special.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AudithoriumSpecialComponent implements OnInit {
 
+  public pageTarget: String;
+  public typeAudithorium: String;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.typeAudithorium = "CFAS";
+
+    if (this.typeAudithorium == "CFAS")
+      this.pageTarget = "https://forms.gle/5VfA4iy2nk7EHjk36";
+    else if (this.typeAudithorium == "CECX")
+      this.pageTarget = "https://forms.gle/2RPsHWcfDistBuFX8";
+    else if (this.typeAudithorium == "Clube")
+      this.pageTarget = "https://forms.gle/ztEy9xRVLKCiBA6Z6";
+    else if (this.typeAudithorium == "Editora")
+      this.pageTarget = "https://forms.gle/QPFkjtDedsrnen5A9";
+
+    console.log(this.pageTarget);
+
   }
 
 }

6aa1aabdb2aff6db1b917d0f3af7a344a792d426
vinicius kremer <vinicius.kremer@gmail.com>
2021-01-28 23:08:49 -0300

    Add conteúdo dinâmico lectures

--- src/app/epics/main/feira/palestras/palestras.component.ts
+++ src/app/epics/main/feira/palestras/palestras.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Lecture } from 'src/app/models/lecture';
+import { DocumentService } from 'src/app/services/document.service';
+import { LectureService } from 'src/app/services/lecture.service';
+import { NavbarService } from 'src/app/services/navbar.service';
 
 @Component({
   selector: 'app-palestras',
   templateUrl: './palestras.component.html',
-  styleUrls: ['./palestras.component.css']
+  styleUrls: ['./palestras.component.css'],
+  providers: [LectureService, DocumentService]
 })
 export class PalestrasComponent implements OnInit {
+  public lectureList: Lecture[] = [];
 
-  constructor() { }
+  constructor(
+    private _lectureService: LectureService,
+    private _documentService: DocumentService,
+    private _navbarService: NavbarService,
+    private _router: Router
+  ) { }
 
   ngOnInit(): void {
+    let epic = JSON.parse(localStorage.getItem("currentEpic"));
+    this._lectureService.getLectures(1, epic._id).subscribe((response) => {
+      this.lectureList = response.lectures;
+      this.loadThumbnails();
+    });
+
+    this._navbarService.setButtonBack(true);
+  }
+
+  loadThumbnails() {
+    this.lectureList.map((lecture) => {
+      console.log(lecture.thumbnail);
+      if (lecture.thumbnail) {
+        this._documentService
+          .getDocument(lecture.thumbnail)
+          .subscribe((response) => {
+            lecture.thumbnail = response.document;
+          });
+      }
+    });
+  }
+
+  goToAudithorium(item: Lecture) {
+    this._router.navigate(["/audithorium", "workshop", item._id]);
   }
 
 }
